2cc3135b6f3ef18275a8d1f2a1e9db536250f6f3
kontaro <moyans@hotmail.com>
2018-07-08 16:56:26 -0400

    crado

---
+++ main.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+
+def createNonogram(n,m):#n and m refers to lenght of matrice
+    nonogram=np.zeros(n,m)
+    return nonogram
+def imprimirNonograma(state):
+
+def leerPistas():
+    listaIzquierda     
