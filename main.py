e018609b8100de492d2d28eaf4e074179e842a19
kontaro <moyans@hotmail.com>
2018-07-08 17:50:27 -0400

    esto

--- main.py
+++ main.py
@@ -2,9 +2,35 @@ import numpy as np
 
 
 def createNonogram(n,m):#n and m refers to lenght of matrice
-    nonogram=np.zeros(n,m)
+    nonogram=np.zeros((n,m))
     return nonogram
 def imprimirNonograma(state):
+    hoal=0;
 
 def leerPistas():
-    listaIzquierda     
+    listaIzquierda=0
+
+def reglas(nonogram,Pistas,n,m):
+    #pistas=Pistas.split()
+    x=0
+    for listP in Pistas:
+
+        if(listP==0):
+            for y in range(m):
+                nonogram[x,y]=1
+        if(listP==m):
+            for y in range(m):
+                nonogram[x,y]=2
+
+        x=x+1
+    return nonogram
+
+
+#def Resolver(nonogram,PLeft,PRight):
+
+
+
+pistas=[0,4,3]
+nonogram=createNonogram(3,4)
+nonogram=reglas(nonogram,pistas,3,4)
+print(nonogram)
